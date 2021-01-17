29009a0f84e557652d1154d468f426c983dcf17f
guidezSeb <sebastien.guidez@lacatholille.fr>
2021-01-17 17:49:51 +0100

    update url

--- conduit/settings.py
+++ conduit/settings.py
@@ -27,7 +27,7 @@ class Config(object):
         'http://localhost:4000',
         'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
         'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
-        'https://tp-devops-final.netlify.app',
+        'https://reverent-swanson-5ad65a.netlify.app',
         os.environ.get('FRONT_PROD_URL', ''),
     ]
     JWT_HEADER_TYPE = 'Token'
